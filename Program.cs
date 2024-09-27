efddc2ece5001e4e6c21abde3515be2ad3e16d95
YendisFish <YendisFish@users.noreply.github.com>
2024-09-26 19:12:32 -0600

    added debug flag to Zyphe

--- Program.cs
+++ Program.cs
@@ -1,21 +1,39 @@
 ﻿using System;
+using Newtonsoft.Json;
 using Zyphe;
 using Zyphe.Parser;
 
 string fle = File.ReadAllText("./Mockups/hello.zp");
 
+bool debug = args.Contains("--debug");
+
 Token[] toks = Lexer.Tokenize(fle);
 toks = Lexer.RemoveWhitespace(toks);
 
-foreach (var token in toks)
+if(debug)
 {
-    Logger.Log(token.type);
-    Logger.Log(token.keyword);
-    Logger.Log(token.value);
-    Logger.Log("--------------------------------------");
+    foreach (var token in toks)
+    {
+        Logger.Log(token.type);
+        Logger.Log(token.keyword);
+        Logger.Log(token.value);
+        Logger.Log("--------------------------------------");
+    }
 }
 
 Parser parser = new Parser(toks);
 parser.Parse();
+
+if(debug)
+{
+    var settings = new JsonSerializerSettings
+    {
+        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+    };
+    
+    string json = JsonConvert.SerializeObject(parser.ast, Formatting.Indented, settings);
+    
+    Console.WriteLine(json);
+}
 
 Console.WriteLine("Done Compiling!");
